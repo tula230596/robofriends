fb77ca34ebcce8c5f18482bb7595b88e3ee8de0c
Tuan Lam Lay <lamlay@Tuans-MacBook-Pro.local>
2020-10-16 01:13:28 -0700

    Updates

---
+++ precache-manifest.eebf25194895f17b2bacc4b9bbde063b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0f45e46f41a4c7f11b93d331e565df",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "878445d6fa89243152ec",
+    "url": "/robofriends/static/css/main.6aac7946.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878445d6fa89243152ec",
+    "url": "/robofriends/static/js/main.2d9da308.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "0efa82f995e38e1d8ee66624294eb9c6",
+    "url": "/robofriends/static/media/Karlin-1Gl52.0efa82f9.ttf"
+  }
+]);
